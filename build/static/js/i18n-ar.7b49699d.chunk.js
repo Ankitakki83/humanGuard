c2575f8d7433c7d2801b648a6ea8f4894e3e3cf8
Ankit <amaurya@scgcontractor.com>
2021-05-22 11:53:11 +0530

    build

--- build/static/js/i18n-ar.7b49699d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonplittra=this.webpackJsonplittra||[]).push([[0],{86:function(t){t.exports=JSON.parse("{}")}}]);
-//# sourceMappingURL=i18n-ar.7b49699d.chunk.js.map
---
+++ build/static/js/i18n-ar.b9840307.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonphumanguard=this.webpackJsonphumanguard||[]).push([[0],{86:function(a){a.exports=JSON.parse("{}")}}]);
+//# sourceMappingURL=i18n-ar.b9840307.chunk.js.map
