3c5643793eea1c0dee834465211954aa781ee416
aaku14n <akarshgdps@gmail.com>
2021-07-24 18:02:09 +0530

    ADding hcange

--- src/Utils/Constants.js
+++ src/Utils/Constants.js
@@ -1,7 +1,8 @@
 export const ARABIC_LANGUAGE = "ar";
 export const ENGLISH_LANGUAGE = "en";
 export const CHANGE_LOCALE = "el";
-export const PUBLIC_ASSETS_PATH = "/assets/images";
+export const PUBLIC_ASSETS_PATH =
+  "https://ankitakki83.github.io/humanGuard/assets/images";
 export const DARK_THEME = "dark";
 export const LIGHT_THEME = "light";
 export const THEME_LOCAL_STORAGE = "theme";
